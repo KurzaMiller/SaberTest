9bd10af64f34033d46d8b5ce110a63128b135492
KurzaMiller <al.kurzenkov@gmail.com>
2023-09-25 17:27:00 +0300

    Метод добавления записей + приведение мотодов к единому виду + заведение списка полей

--- SaberTest/Program.cs
+++ SaberTest/Program.cs
@@ -9,6 +9,7 @@ using Google.Apis.Sheets.v4;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using Google.Apis.Sheets.v4.Data;
+using System.Data;
 
 namespace SaberTest
 {
@@ -22,6 +23,25 @@ namespace SaberTest
 
         static readonly string sourceSheet = "Source";
         static readonly string reportSheet = "Report";
+
+        static readonly string posSourceLeftUp = "A2";
+        static readonly string posSourceRightDown = "L242";
+
+        enum column
+        { 
+            Issuekey,
+            IssueID,
+            IssueType,
+            Status,
+            Priority,
+            Resolution,
+            Assignee,
+            Reporter,
+            Created,
+            Resolved,
+            OriginalEstimate,
+            TimeSpent
+        }
 
         //Объявление переменной сервиса Google Sheets
         static SheetsService sheetService;
@@ -43,22 +63,38 @@ namespace SaberTest
             });
         }
 
-        //Метод для считывания и обработки получаемых с указанной таблицы данных
-        public static void ReadEntries()
+        //Метод для считывания и обработки получаемых с указанной таблицы данных из указанного диапазона
+        public static List<object> ReadEntries(string sheetName, string rangeFrom, string rangeTo)
         {
-            var range = $"{sourceSheet}!A1:A242";
+            var range = $"{sheetName}!{rangeFrom}:{rangeTo}";
             var request = sheetService.Spreadsheets.Values.Get(SpreadsheetID, range);
 
             var responce = request.Execute();
-            var values = responce.Values;
+            var values = (List<object>)responce.Values;
+
             if (values != null && values.Count > 0)
-            {
-                foreach (var row in values)
-                {
-                    Console.WriteLine(row[0]);
-                }
-            }
-            else Console.WriteLine("тут пусто бля");
+                Console.WriteLine("Values are ok");
+            else
+                throw new Exception("!!! MISSING VALUES !!!");
+
+            return values;
+        }
+
+        //Метод для создания списка записей в указанной таблице с ближайшего свободного места в указаном диапазоне
+        public static void CreateEntry(List<object> values, string sheetName, string rangeFrom, string rangeTo)
+        {
+            var range = $"{sheetName}!{rangeFrom}:{rangeTo}";
+            var valueRange = new ValueRange();
+
+            if (values != null && values.Count > 0)
+                valueRange.Values = new List<IList<object>> { values };
+            else
+                throw new Exception("!!! MISSING VALUES !!!");
+
+            var appendRequest = sheetService.Spreadsheets.Values.Append(valueRange, SpreadsheetID, range);
+            appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+
+            var appendResponse = appendRequest.Execute();
         }
 
         private static void Main(string[] args)
@@ -66,7 +102,9 @@ namespace SaberTest
             Console.WriteLine("Hello, World!");
 
             ConnectToGoogle();
-            ReadEntries();
+
+            var values = new List<object>();
+            values = ReadEntries(sourceSheet, posSourceLeftUp, posSourceRightDown);
 
             Console.ReadKey();
         }
